2bb8f7be35992deafbd0ac1f11d9353db20fa99d
Ingye0 <qkrqjarms99@gmail.com>
2026-02-10 10:13:56 +0900

    1009

---
+++ Bronze/Bronze/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace Bronze
+{
+    class Program
+    {
+        static void Main()
+        {
+            int testcase = int.Parse(Console.ReadLine());
+            List<BigInteger> results1 = new List<BigInteger>(testcase);
+
+            for (int i = 0; i < testcase; i++)
+            {
+                var inputs = Console.ReadLine().Split(' ');
+                int a = int.Parse(inputs[0]);
+                int b = int.Parse(inputs[1]);
+
+                BigInteger result = BigInteger.Pow(a, b) % 10;
+                if (result == 0)
+                {
+                    result = 10;
+                }
+
+                results1.Add(result);
+            }
+
+            foreach (BigInteger b in results1)
+            {
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
