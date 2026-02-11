0ed757539415de3d5ad24f00ef6cb5111c7406db
Ingye0 <qkrqjarms99@gmail.com>
2026-02-11 16:46:58 +0900

    0211

---
+++ 0211/0211/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _0211
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            int Longs = input / 4;
+
+            for(int i = 0; i < Longs; i++)
+            {
+                Console.Write("long ");
+            }
+                Console.Write("int");
+        }
+    }
+}
